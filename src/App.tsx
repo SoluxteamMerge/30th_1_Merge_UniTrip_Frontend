020a01f2dafa4db195514201ec6c13c47998b189
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-07-11 14:19:30 +0900

    메인페이지 임시

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import SignupPage from './SignupPage'
-
-import './App.css'
+import React from 'react';
+import './App.css';
+import './LoginPage'
+import './MainPage';
+import MainPage from './MainPage';
 
 function App() {
-
-  return (
-    <>
-      <Header isLoggedIn={false} />
-      <SignupPage />
-    </>
-  )
+  return <MainPage />; // 앱 진입점에서 MainPage 렌더링
 }
 
-export default App
+export default App;
+
---
+++ src/components/ReviewCard.tsx
@@ -0,0 +1,27 @@
+// components/ReviewCard.tsx
+import React from 'react';
+import './ReviewCard.css';
+
+interface ReviewCardProps {
+  title: string;
+  tags: string[];
+  imageUrl: string;
+  summary: string;
+}
+
+export const ReviewCard = ({ title, tags, imageUrl, summary }: ReviewCardProps) => {
+  return (
+    <div className="review-card">
+      <img src={imageUrl} alt="리뷰 이미지" className="card-img" />
+      <div className="card-content">
+        <h3>{title}</h3>
+        <p>{summary}</p>
+        <div className="tags">
+          {tags.map((tag, idx) => (
+            <span key={idx} className="tag">#{tag}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
