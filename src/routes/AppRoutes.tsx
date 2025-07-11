865fb6664ef7d61666210bb8a83a9357ba51fb8c
yakimm12 <kya2415394@gmail.com>
2025-07-11 14:51:25 +0900

    기록한 청춘1

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import YouthTalkBoardPage from "../pages/YouthTalkBoardPage";
 import WritePostPage from "../pages/WritePostPage";
+import RecordedYouthPage from "../pages/RecordedYouthPage";
 
 const AppRoutes = () => (
   <Routes>
+    <Route path="/" element={<RecordedYouthPage />} /> {/*메인 페이지로 교체*/}
+
     <Route path="/youth-talk" element={<YouthTalkBoardPage />} />
     <Route path="/youth-talk/write" element={<WritePostPage />} />
     {/* 다른 페이지 라우트도 여기에 추가 */}
+    <Route path="/recorded-youth" element={<RecordedYouthPage />} />
   </Routes>
 );
 
