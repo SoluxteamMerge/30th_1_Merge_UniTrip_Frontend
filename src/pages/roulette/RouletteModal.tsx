3e867054511c8c5c3239df1a44314edf24c08dbb
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-07-14 10:43:51 +0900

    룰렛 모달 드롭다운 창 구현

--- src/pages/roulette/RouletteModal.tsx
+++ src/pages/roulette/RouletteModal.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RouletteModal.css';
+import closeIcon from "../../assets/close.svg";
+import arrowDown from '../../assets/DropDown.svg';
+import arrowUp from '../../assets/DropUp.svg';
 
 interface Props {
   onClose: () => void;
 }
 
 export default function RouletteModal({ onClose }: Props) {
+  const [selectedOption, setSelectedOption] = useState('룰렛 주제 선정하기');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const options = [
+    '빈칸 채워 룰렛 돌리기',
+    '스몰토크 주제 추천',
+    '레크레이션 추천',
+  ];
+
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    setIsDropdownOpen(false);
+  };
+
   return (
     <div className="modal-overlay">
       <div className="modal-box">
-        <button className="close-btn" onClick={onClose}>✕</button>
-        <h2>룰렛 모달창</h2>
-        {/* 여기에 나중에 룰렛 컴포넌트 들어갈 예정 */}
+        <button className="close-btn" onClick={onClose}>
+          <img src={closeIcon} alt="닫기" />
+        </button>
+
+        <div className="dropdown">
+          <button className="dropdown-btn" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            <span>{selectedOption}</span>
+            <img src={isDropdownOpen ? arrowUp: arrowDown} alt="화살표" className="arrow-icon" />
+          </button>
+          {isDropdownOpen && (
+            <ul className="dropdown-menu">
+              {options.map((option, idx) => (
+                <li key={idx} onClick={() => handleSelect(option)}>
+                  {option}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
+        {/* 여기에 룰렛 컴포넌트 들어갈 예정 */}
       </div>
     </div>
   );
