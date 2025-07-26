89d7493a24b8448c9faef839ace53d104f32fb5e
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-07-26 23:48:52 +0900

    [Deploy] 프론트 배포용 빌드 및 설정 반영

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-console.log('🛠️ HTTPS 설정 적용됨')
+console.log("🛠️ HTTPS 설정 적용됨");
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost-cert.pem'),
-    },
-    host: '0.0.0.0',
-    port: 5173,
-
-  },
-})
+});
