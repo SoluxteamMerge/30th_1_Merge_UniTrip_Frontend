28efe9fab97826ba7cd1a58b6075b9bb3fed8b84
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-07-30 18:22:41 +0900

    api.ts 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,18 @@
 // src/api/api.ts
-//Axios 기본 설정 (모든 API 요청의 기반)
 import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: apiUrl,
-  withCredentials: true, // 필요에 따라
+  withCredentials: true,
 });
 
-// ✅ 모든 요청에 accessToken 자동 추가
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token.trim()}`;  // trim() 추가
     }
     return config;
   },
@@ -22,3 +20,4 @@ api.interceptors.request.use(
 );
 
 export default api;
+
