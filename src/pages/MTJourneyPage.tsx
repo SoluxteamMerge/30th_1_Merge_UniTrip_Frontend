0a1fecfa8966edba43a03a7249cafb6ff8cf1392
jsm1129 <jamja0513@sookmyung.ac.kr>
2025-07-31 00:23:00 +0900

    게시글 수정

--- src/pages/MTJourneyPage.tsx
+++ src/pages/MTJourneyPage.tsx
@@ -8,10 +8,10 @@ import starWishFillIcon from "../assets/module/star_wish_fill.svg";
 import { getReviews, ReviewItem } from '../api/Review/getReviewsApi';
 
 const categoryToBoardType: Record<string, string> = {
-  "MT여정지도": "MT/LT",
+  "MT여정지도": "MT_LT",
   "함께해요-동행구해요": "동행모집",
   "함께해요-번개모임": "모임구인",
-  "함께해요-졸업/휴학여행": "졸업/휴학여행",
+  "함께해요-졸업/휴학여행": "졸업_휴학여행",
   "함께해요-국내학점교류": "국내학점교류",
   "함께해요-해외교환학생": "해외교환",
 };
