d69bf807673d0255c9e7acb572e491f2d722c6ba
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-07-30 16:00:51 +0900

    config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
     host: true,
     port: 5173,
     allowedHosts: ["unitrip.duckdns.org"], //수정 부분
