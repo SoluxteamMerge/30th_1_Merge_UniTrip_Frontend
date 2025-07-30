a75d318585a3e4b3dca16e41dc7f28f67141aeb2
yakimm12 <kya2415394@gmail.com>
2025-07-31 02:32:44 +0900

    searchReviews.ts 수정

--- src/api/search/searchReviews.ts
+++ src/api/search/searchReviews.ts
@@ -28,8 +28,7 @@ export const searchReviews = async (
   sort: string = "popular" // 기본 정렬: 인기순
 ): Promise<SearchReviewResponse> => {
   const response = await axios.get(`${BASE_URL}/api/reviews/search`, {
-    //params: { keyword, sort },
-    params: { keyword: encodeURIComponent(keyword), sort },
+    params: { keyword, sort },
     headers: { Authorization: token },
   });
   console.log("📦 [searchReviews] 전체 response:", response);
