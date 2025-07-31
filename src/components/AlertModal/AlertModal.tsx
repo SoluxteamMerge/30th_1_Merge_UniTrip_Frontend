0a87641984b8a98c5a0751036364363ffc929357
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-08-01 00:36:35 +0900

    비로그인시 상세페이지 조회 불가

--- src/components/AlertModal/AlertModal.tsx
+++ src/components/AlertModal/AlertModal.tsx
@@ -4,22 +4,29 @@ import React from 'react';
 import './AlertModal.css';
 import closeIcon from '../../assets/close.svg';
 
-
 interface AlertModalProps {
   message: React.ReactNode;
   onClose: () => void;
   onConfirm?: () => void;
 }
 
-const AlertModal: React.FC<AlertModalProps> = ({ message, onClose }) => {
+const AlertModal: React.FC<AlertModalProps> = ({ message, onClose, onConfirm }) => {
+  const handleConfirm = () => {
+    if (onConfirm) {
+      onConfirm();
+    } else {
+      onClose();
+    }
+  };
+
   return (
     <div className="alert-overlay" onClick={onClose}>
       <div className="alert-box" onClick={(e) => e.stopPropagation()}>
         <button className="alert-close-button" onClick={onClose}>
-          <img src={closeIcon} alt="닫기"/>
+          <img src={closeIcon} alt="닫기" />
         </button>
         <div style={{ color: 'black', textAlign: 'center' }}>{message}</div>
-        <button className="alert-ok-btn" onClick={onClose}>확인</button>
+        <button className="alert-ok-btn" onClick={handleConfirm}>확인</button>
       </div>
     </div>
   );
